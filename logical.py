d77a96570e3b94c0d9a81d56584ee457ddf44971
Ahmet Kıyran <116555714+ahmetkiyran@users.noreply.github.com>
2025-06-28 14:20:35 +0300

    python

---
+++ logical.py
@@ -0,0 +1,25 @@
+from numpy import result_type
+
+
+x = 5
+
+result = 5 < x < 10
+
+#and 
+
+result = x > 5 and x <10 
+
+print(result)
+
+# or 
+
+result = x > 4 or x <10
+
+print(result)
+
+
+# not 
+
+result = not(x >0)
+print(result)
+
