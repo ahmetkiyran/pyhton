d6c50558016dcd6a41096b1b331953b7d74c432b
Ahmet Kıyran <116555714+ahmetkiyran@users.noreply.github.com>
2025-06-29 15:41:32 +0300

    python

---
+++ if-else.py
@@ -0,0 +1,12 @@
+"""if 3 > 2:
+    print("hoşgeldiniz")"""
+
+"""if True:
+    print("hoşgeldiniz")"""
+   
+isLoggedin  = False
+if isLoggedin:
+    print("hoşgeldiniz")
+else:
+    print("giriş yapılamadı")        
+        
