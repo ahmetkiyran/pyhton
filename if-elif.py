341724015ebacb20fd077adabc7bb43cc809158c
Ahmet Kıyran <116555714+ahmetkiyran@users.noreply.github.com>
2025-06-30 00:12:19 +0300

    python

---
+++ if-elif.py
@@ -0,0 +1,9 @@
+x = 20
+y = 20
+
+if x > y:
+    print("x y den büyüktür")
+elif x == y:
+    print("x ve y eşittir") 
+else:
+    print("y x den büyüktür")       
---
+++ if-else-demo.py
@@ -0,0 +1,37 @@
+# kullanıcıdan isim yaş ve eğitim bilgilerini isteyip ehliyet alabilme
+#durumunu kontrol ediniz. ehliyet alma koşulu en az 18 ve eğitim
+#durumu lise ya da üniversite olmalıdır.
+
+"""isim = str(input("isim: "))
+yas = int(input("yaşınız: "))
+stateEgitim = str(input("eğitim durumunuz: "))
+
+if yas >= 18 and (stateEgitim == "lise" or stateEgitim == "üniversite"):
+    print("ehliyet alma hakkınız mevcuttur")
+else:
+    print("ehliyet alma hakkınız yoktur.")"""
+
+
+#bir öğrencinin 2 yazılı bir sözlü notunu alıp hesaplanan ortalmaya
+# göre not aralığına karşılık gelen not bilgisini yazdırınız
+
+"""yazılı1 = float(input("1.yazılı notunuzu giriniz: "))
+yazılı2 = float(input("2.yazılı notunuzu giriniz: "))
+sozlu = float(input("sözlü notunuzu giriniz: "))  
+
+ortalama = (yazılı1 + yazılı2 + sozlu)/3
+
+if ortalama > 0 and ortalama <= 24:
+    print("not: 0")
+elif ortalama >= 25 and ortalama <= 44:
+    print("not: 1")
+elif ortalama >= 45 and ortalama <= 54:
+    print("not: 2")
+elif ortalama >= 55 and ortalama <= 69:
+    print("not: 3")
+elif ortalama >= 70 and ortalama <= 84:
+    print("not: 4")
+else:
+    print("not: 5")"""
+
+
