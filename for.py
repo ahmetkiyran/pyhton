9f4d9fd0e253695f00479e57c86fc9eb22a2f33c
Ahmet Kıyran <116555714+ahmetkiyran@users.noreply.github.com>
2025-07-08 01:06:28 +0300

    python

---
+++ for.py
@@ -0,0 +1,18 @@
+numbers = [0,1,2,3,4,5]
+
+for num in numbers: # değişkeni kendimiz belirliyoruz
+    print(num)
+
+
+names = ["ahmet","ali","sadık","vitalin"] 
+for name in names:
+    print(name)   
+
+tuple = [(1,2),(2,3),(3,4),(4,5)]
+
+for a,b in tuple:
+    print(a,b)    
+
+d = {"k1": 1,"k2": 2, "k3": 3}
+for key,value in d.items():
+    print(key,value)    
