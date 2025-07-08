4247d04774a347d9a206a49b656235226637b680
Ahmet Kıyran <116555714+ahmetkiyran@users.noreply.github.com>
2025-07-08 04:22:45 +0300

    python

---
+++ while-demo.py
@@ -0,0 +1,34 @@
+sayılar = [1,3,5,7,9,12,19,21]
+
+#sayılar listesini while ile ekrana yazdırınız.
+"""i = 0
+while i < len(sayılar):
+    print(sayılar[i])
+    i = i + 1"""
+# başlangı. ve bitiş değerlerini kullanıcıdan alıp aradaki tüm tek sayıları ekrana yazdırın.
+
+"""a = int(input("başlangıç değerini giriniz: "))
+b = int(input("başlangıç değerini giriniz: "))
+i = a 
+while  i<b:
+    i = i + 1
+    if i % 2 == 1:
+        print(i)"""
+
+
+# 1-100 arasındaki sayıları azalan şekilde yazdırın
+"""i = 100
+while i>= 1:
+    i = i -1
+    print(i)"""
+         
+# kullanıcıdan alacağınız 5 sayıyı ekranda sıralı bir şekilde yazdırın.
+
+"""numbers = []
+i = 0
+while i < 5:
+    sayı = int(input("sayı: "))
+    numbers.append(sayı)
+    i = i + 1
+   
+print(numbers)"""
