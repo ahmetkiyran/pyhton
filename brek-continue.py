a2b1cd188117bc1f4221d1e7bfbf7d515a6048bd
Ahmet Kıyran <116555714+ahmetkiyran@users.noreply.github.com>
2025-07-11 01:42:34 +0300

    pyhton

---
+++ brek-continue.py
@@ -0,0 +1,15 @@
+name = "ahmet kıyran"
+
+for i in name:
+    if i == "ı":
+        break
+    print(i)
+    
+x = 0
+
+while x < 10:
+    if x == 4:
+        break
+    print(x)
+    x +=1    
+    
