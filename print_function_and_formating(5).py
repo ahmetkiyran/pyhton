dc5c493e43765464c304bfcc8235c1c2d82c8a58
ahmet kiyran <ahmet.kiyran1@gmail.com>
2025-10-02 14:22:25 +0300

    changes update

---
+++ print_function_and_formating(5).py
@@ -0,0 +1,3 @@
+# \n -> alt alta yazdırmak istediğimizde stringte bunu kullanabiliriz. 
+# \t bir tab kadar arada boşluk bırakarak yazar
+# sep = " " araya ne koyarsak bu parametrede onu koyar ve kullanabiliriz.
