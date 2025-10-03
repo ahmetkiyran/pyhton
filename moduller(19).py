1293b056772a15846479e1c2b043778e73044926
ahmet kiyran <ahmet.kiyran1@gmail.com>
2025-10-04 00:00:58 +0300

    third day

---
+++ moduller(19).py
@@ -0,0 +1,11 @@
+#import math # burda modülü kullanmak için import ediyoruz yani ekliyoruz
+"""
+from math import * -> bu math kütüpündeki her şeyi import ettiğimiz anlamına gelir 
+"""
+from math import * # sağlıklı değil çünkü python burda hangisi sonda ekliyse onu çalıştırıyor.
+#help(math) # bu modül hakkında bilgileri öğrenmek için yazarız
+
+print(factorial(5))
+
+print(ceil(5.7))
+
