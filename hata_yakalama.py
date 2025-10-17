9ccf274280ab7194619de3e31b7ace2f459ffae1
ahmet kiyran <ahmet.kiyran1@gmail.com>
2025-10-17 18:49:38 +0300

    python a devam ediyorum

--- hata_yakalama.py
+++ hata_yakalama.py
@@ -33,3 +33,44 @@ try:
 except:
     print("bir hata oluştu")
 print("bloklar sona erdi!")
+
+try:
+    a = int(input("sayı 1: "))
+    b = int(input("sayı 2: "))
+    print("cevap: ",a/b)
+except ValueError:
+    print("lütfen inputu doğru girin. ")
+except ZeroDivisionError:
+    print("bir sayı sıfıra bölünemez. ")
+print("bloklar sona erdi. ")
+
+#finally blokları
+#mutlaka çalışması gereken kodların bulunması gerkeen bloklar
+#hata olsa da olmasa da çalışması gerek
+try:
+    a = int(input("sayı 1: "))
+    b = int(input("sayı 2: "))
+    print("cevap: ",a/b)
+except ValueError:
+    print("lütfen inputu doğru girin. ")
+except ZeroDivisionError:
+    print("bir sayı sıfıra bölünemez. ")
+finally:
+    print("burası çalıştı...")
+print("bloklar sona erdi. ")
+
+#hata fırlatma
+
+def terscevir(s):
+    if(type(s) != str):
+        raise ValueError("lütfen bir string değer giriniz!")
+    else:
+        return s[::-1]
+    
+    
+try:
+    print(terscevir(12))
+except ValueError:
+    print("fonksiyon hata verdi.")
+
+#kendi fonksiyonlarımızda hata fırlatmayı bu şekilde yapabiliyoruz.
